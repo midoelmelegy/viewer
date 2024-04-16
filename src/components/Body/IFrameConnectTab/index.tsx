9e924be9e065c5bb0baed720d735418950944f65
TomCat <midoelmelegy@gmail.com>
2024-04-17 00:15:42 +0800

    Update index.tsx

--- src/components/Body/IFrameConnectTab/index.tsx
+++ src/components/Body/IFrameConnectTab/index.tsx
@@ -62,7 +62,7 @@ function IFrameConnectTab({
             <Input
               pr="3.5rem"
               bg={"brand.lightBlack"}
-              placeholder="https://pancakeswap.finance/prediction?token=BNB&chain=bsc"
+              placeholder="https://app.uniswap.org/"
               aria-label="dapp-url"
               autoComplete="off"
               value={inputAppUrl}
@@ -113,7 +113,7 @@ function IFrameConnectTab({
       >
         {appUrl && (
           <Box
-            as="embed"
+            as="iframe"
             w={{
               base: "22rem",
               sm: "45rem",
